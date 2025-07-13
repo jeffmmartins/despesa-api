a296bf4e9c608ad5004a5cce450005792b49fac9
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-13 16:47:08 -0300

    Feat: Iniciando projeto despesa

---
+++ src/main/java/com/github/jeffmmartins/despesas_API/ApiDeDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.github.jeffmmartins.despesas_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeDespesasApplication.class, args);
+	}
+
+}
