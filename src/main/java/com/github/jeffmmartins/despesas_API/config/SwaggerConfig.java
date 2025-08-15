eb066db1365cf06f9468a56562053cf65a19a719
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-14 21:13:21 -0300

    Feat: SwaggerConfig criado

---
+++ src/main/java/com/github/jeffmmartins/despesas_API/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.github.jeffmmartins.despesas_API.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customAPI(){
+        return new OpenAPI().info(new Info().title("Gerenciamento de Despesas")
+                        .description("Uma API RESTful para cadastrar, consultar, atualizar e deletar despesas.")
+                        .contact(new Contact()
+                                .name("Jefferson Martins Mendes")
+                                .url("https://github.com/jeffmmartins/despesa-api")
+                                )
+                        .version("1.0.0"));
+    }
+}
